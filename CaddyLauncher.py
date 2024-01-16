07a85aa96e6219955780028181c19656ed480183
qin.batista <qin.batista@outlook.com>
2024-01-16 13:29:04 -0800

    add log

--- CaddyLauncher.py
+++ CaddyLauncher.py
@@ -6,17 +6,25 @@ import time
 from socket import *
 class CaddyLauncher:
     def __init__(self):
-        pass
+        self.__file_path = "/v2ray_server.txt"
 
     def _Caddy(self):
         v2ray_address = os.environ.get('V2RAY_ADDRESS')
         while True:
             time.sleep(10)
-            print(requests.get("https://checkip.amazonaws.com").text.strip()+" " + gethostbyname(v2ray_address))
+            self.__log(requests.get("https://checkip.amazonaws.com").text.strip()+" " + gethostbyname(v2ray_address))
             if requests.get("https://checkip.amazonaws.com").text.strip() == gethostbyname(v2ray_address):
                 break
         p = subprocess.Popen("caddy run --config /etc/caddy/Caddyfile", universal_newlines=True, shell=True,)
         p.wait()
+
+    def __log(self, result):
+        with open(self.__file_path, "a+") as f:
+            f.write(result+"\n")
+        if os.path.getsize(self.__file_path) > 1024*128:
+            with open(self.__file_path, "r") as f:
+                content = f.readlines()
+                os.remove(self.__file_path)
 
 if __name__ == "__main__":
     sf = CaddyLauncher()
