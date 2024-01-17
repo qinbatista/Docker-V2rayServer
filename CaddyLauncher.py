f73e7a9e162d5577ab3a94fdc0c8213470232720
qin.batista <qin.batista@outlook.com>
2024-01-16 19:40:50 -0800

    save

--- CaddyLauncher.py
+++ CaddyLauncher.py
@@ -4,6 +4,8 @@ import subprocess
 import requests
 import time
 from socket import *
+
+
 class CaddyLauncher:
     def __init__(self):
         self.__file_path = "/v2ray_server.txt"
@@ -12,12 +14,22 @@ class CaddyLauncher:
         v2ray_address = os.environ.get('V2RAY_ADDRESS')
         while True:
             time.sleep(10)
-            self.__log(requests.get("https://checkip.amazonaws.com").text.strip()+" " + gethostbyname(v2ray_address))
+            self.__log(requests.get(
+                "https://checkip.amazonaws.com").text.strip()+" " + gethostbyname(v2ray_address))
             if requests.get("https://checkip.amazonaws.com").text.strip() == gethostbyname(v2ray_address):
                 break
             time.sleep(10)
-        p = subprocess.Popen("caddy run --config /etc/caddy/Caddyfile", universal_newlines=True, shell=True,)
-        p.wait()
+            process = subprocess.Popen(
+                "caddy run --config /etc/caddy/Caddyfile",
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                universal_newlines=True
+            )
+            stdout, stderr = process.communicate()
+            result = stdout + stderr
+            self.__log(result)
+            process.wait()
 
     def __log(self, result):
         with open(self.__file_path, "a+") as f:
@@ -26,6 +38,7 @@ class CaddyLauncher:
             with open(self.__file_path, "r") as f:
                 content = f.readlines()
                 os.remove(self.__file_path)
+
 
 if __name__ == "__main__":
     sf = CaddyLauncher()
